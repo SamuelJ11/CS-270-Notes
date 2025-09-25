22f47933a4ad428a6ec073088c2e9888e8c87187
SamuelJ11 <sjab225@uky.edu>
2025-09-25 02:15:00 +0000

    H3 done (i think)

--- 2025-09-22/Homework3.c
+++ 2025-09-22/Homework3.c
@@ -54,6 +54,7 @@ int foo(char *rdi)
 */
 
 //Problem 0:4
+/*
 int foo(int edi, int esi)
 {
     int retval = 0;
@@ -73,3 +74,53 @@ int foo(int edi, int esi)
     retval = ecx;
     return retval;
 }
+*/
+
+/*
+bazz:
+cmpq $2, %rsi        // 3 > 2           // 2 > 2 !!
+jg .L2
+movq (%rdi), %rax                       // %rax = 0             **%rdi = &y
+addq $1, %rax                           // %rax = 0 + 1 = 1
+ret                                      
+.L2:
+subq $8, %rsp        // γ = γ - 8  
+subq $1, %rsi        // %rsi = 2
+call bazz            // γ = γ - 8
+addq $1, %rax
+addq $8, %rsp
+ret
+
+bar:
+pushq %rbp         // β
+pushq %rbx         // a = 10
+subq $8, %rsp      // γ = γ - 8
+movq %rdi, %rbx    // a = &y
+movq (%rsi), %rbp  // β = 10             
+movl $3, %esi      // %rsi = 3
+call bazz          // γ = γ - 8
+addq (%rbx), %rax
+movq %rax, (%rbx)
+addq %rbp, %rax
+addq $8, %rsp
+popq %rbx
+popq %rbp
+ret
+
+foo:
+pushq %rbx          // a
+subq $16, %rsp      //
+movq %rdi, %rax     /// %rax = 10
+movq %rsi, %rdi     // %rdi = &y
+xorl %edx, %edx     // %edx = 0
+movq (%rsi), %rbx   // a = 0
+addq %rbx, %rax     // a = 0 + 10 = 10
+movq %rax, (%rsp)   // *(γ - 16) = 10
+movq %rsp, %rsi     // %rsi = γ - 16, where *(γ - 16) = 10
+call bar            // *(γ - 16) = address of addq %rbx, %rax
+addq %rbx, %rax
+jne .L12
+addq $16, %rsp
+popq %rbx
+ret
+*/
