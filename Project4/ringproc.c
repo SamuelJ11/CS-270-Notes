be62217b9da27048102995f63b4c10b216726472
SamuelJ11 <sjab225@uky.edu>
2025-12-01 22:45:13 +0000

    save project 4 progress

--- Project4/ringproc.c
+++ Project4/ringproc.c
@@ -4,24 +4,22 @@
 volatile sig_atomic_t IAMLEADER = 0;
 volatile sig_atomic_t sig_received = 0;
 int numcycles = -1;
+long cycle_value = 0;
+char buf[5];
 
-// implement a function to report fatal system-level errors and exit
 void unix_error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
 }
 
-// create the actual signal handler
 void mysighandler(int signal)
 {
     sig_received = 1;
 }
 
-// define a new type called 'sighandler_t': this new type is an alias for a pointer to a function that takes an int and returns void
 typedef void (*sighandler_t)(int);
 
-// create a wrapper function called Signal() that calls sigaction() for us
 void Signal(int signum, sighandler_t handler)
 {
     struct sigaction new_action;
@@ -37,30 +35,103 @@ void Signal(int signum, sighandler_t handler)
 
 int main(int argc, char *argv[]) 
 {
-    // extract the file descriptors from *pargv
+    // STEP 1: EXTRACT THE FILE DESCRIPTORS FROM *PARGV
+
     int input_fd = atoi(argv[READFDARG]);
     int output_fd = atoi(argv[WRITEFDARG]);
     int ready_fd = atoi(argv[READYFDARG]);
 
-    // extract the ring index from the environment variable (PROCNUMKEY defined as "childindx")
+    // STEP 2: GET MY INDEX FROM THE ENVIRONMENT
+
     int myIndex = atoi(getenv(PROCNUMKEY));
 
-    // determine if I am the leader
+    // STEP 3: DETERMINE IF LEADER AND INSTALL HANDLER
+  
     char *numcycles_env = getenv(NUMCYCSKEY);
     if (numcycles_env != NULL) 
     {
         IAMLEADER = 1;             
         numcycles = atoi(numcycles_env); 
+
+        Signal(SIGUSR1, mysighandler);
     } 
     else 
     {
         IAMLEADER = 0;             
     }    
+    
+    // STEP 4: INDICATE TO THE RING DRIVER THAT THIS PROCESS IS READY
 
-    // install signal handler if I am the leader
-    if (IAMLEADER == 1)
+    write(ready_fd, &myIndex, 1);
+    close(ready_fd);
+
+    // STEP 5: WAIT FOR SIGURS1 TO BE DELIVERED, PROMPT USER FOR CYCLE VALUE
+
+    char* value = NULL;
+    if (IAMLEADER)
     {
-        // install the handler
-        Signal(SIGUSR1, mysighandler);
-    }       
+        sigset_t mymask, mtmask, oldmask;
+        sigfillset(&mymask); sigemptyset(&mtmask);
+        if (sigprocmask(SIG_BLOCK, &mymask, &oldmask) < 0)
+        {
+            unix_error("sigprocmask error: ");
+        }
+
+        while (sig_received == 0)
+        {
+            // unblock signals and pause this thread
+            sigsuspend(&mtmask); 
+            /* signal arrived - restore previous signal mask */
+            if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+            {
+                unix_error("sigprocmask error: ");
+            }
+        }
+
+        printf("Please enter an integer in [1,9999] to circulate: ");
+        value = fgets(buf, sizeof(buf), stdin);
+        if (value == NULL)
+        {
+            printf("no input recieved, exiting.\n");
+            exit(0);
+        }
+        cycle_value = atoi(value);
+    }
+
+    // STEP 6: WHILE LOOPING, READ A LONG FROM THE INPUT PIPE FILE DESCRIPTOR
+
+    long cycle_iterator = 0;
+    long check;
+    if (IAMLEADER) 
+    {
+        write(output_fd, &cycle_value, sizeof(long));  // start the ring
+    }
+
+    while(1)
+    {
+        // increment the value read from the predecessor 
+        check = read(input_fd, &cycle_value, sizeof(long));    
+        if (check == 0)
+        {
+            close(output_fd);
+            exit(0);
+        }
+        // report my value and that it was received
+        report(myIndex, cycle_value);
+
+        if (IAMLEADER)
+        {
+            cycle_iterator++;
+            if (cycle_iterator > numcycles)
+            {
+                close(output_fd);
+                exit(0);
+            }            
+        }
+
+        cycle_value++;
+        write(output_fd, &cycle_value, sizeof(long));
+    }
+
+    return 0;
 }
